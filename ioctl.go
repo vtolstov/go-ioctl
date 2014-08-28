fba8f0546db7b520062ded0f824ff750edf944bd
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-08-28 18:45:20 +0400

    add some more ioctls
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- ioctl.go
+++ ioctl.go
@@ -2,10 +2,7 @@
 
 package ioctl
 
-import (
-	"syscall"
-	"unsafe"
-)
+import "syscall"
 
 // Generic ioctl constants
 const (
@@ -75,13 +72,22 @@ func IOCTL(fd, name, data uintptr) error {
 }
 
 // BlkPart send blkpart ioctl to fd
-func BlkPart(fd uintptr) error {
-	BLKRRPART := IO(0x12, 95)
-	var r int
-	return IOCTL(fd, BLKRRPART, uintptr(unsafe.Pointer(&r)))
+func BlkRRPart(fd uintptr) error {
+	return IOCTL(fd, IO(0x12, 95), uintptr(0))
 }
 
 // BlkPg send blkpg ioctl to fd
-func BlkPg(fd uintptr) error {
+func BlkPg(fd, data uintptr) error {
+	return IOCTL(fd, IO(0x12, 105), data)
 	return nil
+}
+
+// Fitrim send fitrim ioctl to fd
+func Fitrim(fd, data uintptr) error {
+	return IOCTL(fd, IOWR('X', 121, uintptr(0)), data)
+}
+
+// Firfreeze send firfreeze ioctl to fd
+func Firfreeze(fd uintptr) error {
+	return IOCTL(fd, IOWR('X', 119, uintptr(0)), data)
 }
